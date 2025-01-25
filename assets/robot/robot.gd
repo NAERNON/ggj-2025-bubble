52488ef083b73e6687d8dde4e3bf1f0c09ca3f70
NAERNON <naernon@icloud.com>
2025-01-25 19:23:26 +0100

    Blower arm added

--- assets/robot/robot.gd
+++ assets/robot/robot.gd
@@ -3,6 +3,11 @@ extends Node3D
 @export var left_wheel_speed: float = 0.0
 @export var right_wheel_speed: float = 0.0
 
+@export var head_rotation: Vector2:
+    set(new_value): 
+        head_rotation = new_value
+        print("ok")
+
 @export_group("Meshes")
 
 @export var head: Node3D
@@ -21,6 +26,7 @@ var _big_wheel_rot_factor: float = 0.5
 
 func _process(delta: float) -> void:
     _rotate_wheels(delta)
+    head.rotation = Vector3(head_rotation.x, 0, head_rotation.y)
 
 func _rotate_wheels(delta: float) -> void:
     left_small_wheel_1.rotate_x(delta * left_wheel_speed)
@@ -29,6 +35,3 @@ func _rotate_wheels(delta: float) -> void:
     right_small_wheel_1.rotate_x(delta * right_wheel_speed)
     right_small_wheel_2.rotate_x(delta * right_wheel_speed)
     right_big_wheel.rotate_x(delta * right_wheel_speed * _big_wheel_rot_factor)
-
-func set_head_rotation(rot: Vector3) -> void:
-    head.rotation = rot
