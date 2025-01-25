1fa3f239b256ae74d9c211a30db81e34c290a749
NAERNON <naernon@icloud.com>
2025-01-25 19:31:56 +0100

    Head rotation

--- assets/robot/robot.gd
+++ assets/robot/robot.gd
@@ -3,10 +3,7 @@ extends Node3D
 @export var left_wheel_speed: float = 0.0
 @export var right_wheel_speed: float = 0.0
 
-@export var head_rotation: Vector2:
-    set(new_value): 
-        head_rotation = new_value
-        print("ok")
+@export var head_rotation: Vector2
 
 @export_group("Meshes")
 
@@ -26,7 +23,7 @@ var _big_wheel_rot_factor: float = 0.5
 
 func _process(delta: float) -> void:
     _rotate_wheels(delta)
-    head.rotation = Vector3(head_rotation.x, 0, head_rotation.y)
+    _rotate_head(delta)
 
 func _rotate_wheels(delta: float) -> void:
     left_small_wheel_1.rotate_x(delta * left_wheel_speed)
@@ -35,3 +32,7 @@ func _rotate_wheels(delta: float) -> void:
     right_small_wheel_1.rotate_x(delta * right_wheel_speed)
     right_small_wheel_2.rotate_x(delta * right_wheel_speed)
     right_big_wheel.rotate_x(delta * right_wheel_speed * _big_wheel_rot_factor)
+    
+func _rotate_head(delta: float) -> void:
+    head.rotation.x = clampf(head_rotation.x, -0.5, 0.72)
+    head.rotation.y = head_rotation.y
