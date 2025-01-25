4aa819a7c1c02f82687e25ac6b99d1f67afb16c7
NAERNON <naernon@icloud.com>
2025-01-25 17:04:24 +0100

    Robot import started

---
+++ assets/robot/robot.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+@export var left_wheel_speed: float = 0.0
+@export var right_wheel_speed: float = 0.0
+
+@export_group("Meshes")
+
+@export var left_small_wheel_1: Node3D
+@export var left_small_wheel_2: Node3D
+@export var left_big_wheel: Node3D
+@export var right_small_wheel_1: Node3D
+@export var right_small_wheel_2: Node3D
+@export var right_big_wheel: Node3D
+
+var _big_wheel_rot_factor: float = 0.5
+
+func _process(delta: float) -> void:
+    _rotate_wheels(delta)
+
+func _rotate_wheels(delta: float) -> void:
+    left_small_wheel_1.rotate_x(delta * left_wheel_speed)
+    left_small_wheel_2.rotate_x(delta * left_wheel_speed)
+    left_big_wheel.rotate_x(delta * left_wheel_speed * _big_wheel_rot_factor)
+    right_small_wheel_1.rotate_x(delta * right_wheel_speed)
+    right_small_wheel_2.rotate_x(delta * right_wheel_speed)
+    right_big_wheel.rotate_x(delta * right_wheel_speed * _big_wheel_rot_factor)
