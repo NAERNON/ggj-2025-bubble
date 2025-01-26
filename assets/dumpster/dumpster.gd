b426e76102163dd89a4343568eca644751c81d30
NAERNON <naernon@icloud.com>
2025-01-26 12:43:57 +0100

    Dumpster animation added

--- assets/dumpster/dumpster.gd
+++ assets/dumpster/dumpster.gd
@@ -1,8 +1,15 @@
 class_name Dumpster extends Node3D
 
-@export var is_speaking: bool = false
+@export var is_speaking: bool = false:
+    set(new_value):
+        is_speaking = new_value
+        if new_value: start_speaking()
+        else: end_speaking()
 
-@export var top_mouse_node: Node3D
+@export var animation_player: AnimationPlayer
 
-func _process(delta: float) -> void:
-    pass
+func start_speaking() -> void:
+    animation_player.play("dumpster_mouse_speaking")
+
+func end_speaking() -> void:
+    animation_player.stop()
